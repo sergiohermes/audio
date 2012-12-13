31fbeceb2b6e564ac38a9825d1dcb6341b3419f1
Sergio Ernane Muhnos Hermes <hermes.sergio@gmail.com>
2012-12-13 18:46:00 -0200

    Atualizado o arquivo index, adicionado os objetivos do projeto

--- public/index.php
+++ public/index.php
@@ -6,5 +6,14 @@
 </head>
 <body>
 	<h1>Sistema de audio</h1>
+	<br />
+	Objetivo do Projeto
+	<ul>
+		<li>Objetivo 1</li>
+		<li>Objetivo 2</li>
+		<li>Objetivo 3</li>
+		<li>Objetivo 4</li>
+		<li>Objetivo 5</li>
+	</ul>	
 </body>
 </html>
